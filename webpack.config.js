59c142a9c958d02be2578fc21bc47f3511810af8
amine benahmed <amineforfriends@gmail.com>
2019-02-02 19:07:34 +0100

    testing how to create a modules in html with webpack.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin= require("html-webpack-plugin");
+
+module.exports={
+  module:{
+    rules:[
+       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
